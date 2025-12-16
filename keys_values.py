dbdf86de1b25e97bc91b7594b7f89119073fc212
git-trial <risabniranjan@gmail.com>
2025-12-16 21:02:59 +0545

    keys_values

---
+++ keys_values.py
@@ -0,0 +1,10 @@
+#Access and print all keys and values.
+student = {
+    "name": "Risab Niranjan",
+    "class": "12th",
+    "roll": 23,
+    "marks": 88
+}
+
+print(student.keys())
+print(student.values())
