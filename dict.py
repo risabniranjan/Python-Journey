88bfab60140f404a196c72680192250537617edc
git-trial <risabniranjan@gmail.com>
2025-12-16 20:50:59 +0545

    dict

---
+++ dict.py
@@ -0,0 +1,9 @@
+#Create a dictionary for a student with:name, class, roll, marks
+student = {
+    "name": "Risab Niranjan",
+    "class": "12",
+    "roll": 23,
+    "marks": 88
+}
+
+print(student)
